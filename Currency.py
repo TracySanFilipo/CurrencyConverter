0d897e16d67ff0fa0d41c42f102990d2efe1f390
TracySanFilipo <tracysanfilipo@gmail.com>
2016-11-25 17:21:08 -0500

    Two currency instances with the same values are equal

--- Currency.py
+++ Currency.py
@@ -4,3 +4,6 @@ class Currency:
     def __init__(self, nation, value):
         self.nation = nation
         self.value = value
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
--- Currency_tests.py
+++ Currency_tests.py
@@ -4,9 +4,14 @@ from Currency import Currency
 
 class TestCurrency(unittest.TestCase):
 
-    def test_currency_amount_and_code(self):
+    def test_currency_instance(self):
         one_dollar = Currency('usd', 1)
         self.assertIsInstance(one_dollar, Currency)
+
+    def test_currency_amount_and_code(self):
+        two_dollar = Currency('usd', 2)
+        second_dollar = Currency('usd', 2)
+        self.assertEqual(two_dollar, second_dollar)
 
 
 if __name__ == '__main__':
