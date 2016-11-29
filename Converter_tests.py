818dc0f875e21dbb3b9f8cba9e6313682e35fa29
TracySanFilipo <tracysanfilipo@gmail.com>
2016-11-29 18:35:38 -0500

    Converter returns identical currency when convert currency to its own code

--- Converter_tests.py
+++ Converter_tests.py
@@ -1,14 +1,20 @@
 import unittest
 from CurrencyConverter import Converter
+from Currency import Currency
 # from CurrencyConverter import UnknownCurrencyCodeError
 
 
 class TestConverter(unittest.TestCase):
 
     def test_converter_has_dictionary(self):
-        converter = Converter({'usd': 1, 'eur': 0.94})
-        self.assertIsInstance(converter, Converter)
+        converter_init = Converter({'usd': 1, 'eur': 0.94})
+        self.assertIsInstance(converter_init, Converter)
 
+    def test_converter_makes_same_currency_the_same(self):
+        converter_init = Converter({'usd': 1, 'eur': 0.94})
+        one_dollar = Currency('usd', 1)
+        other_dollar = converter_init.convert(Currency('usd', 1), 'usd')
+        self.assertEqual(one_dollar, other_dollar)
 
 if __name__ == '__main__':
     unittest.main()
--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,3 +1,11 @@
+from Currency import Currency
+
 class Converter():
     def __init__(self, dictionary):
         self.dictionary = dictionary
+
+    def convert(self, currency, currency_code):
+        self.currency = currency
+        self.currency_code = currency_code
+        new_value = (currency.value * self.dictionary[currency_code])/self.dictionary[currency.nation]
+        return Currency(currency_code, new_value)
