0ca89e429cec612188c7746082456a0f3bd260db
TracySanFilipo <tracysanfilipo@gmail.com>
2016-11-29 17:55:40 -0500

    Converter exists

--- Converter_tests.py
+++ Converter_tests.py
@@ -1,9 +1,13 @@
 import unittest
+from CurrencyConverter import Converter
+# from CurrencyConverter import UnknownCurrencyCodeError
+
 
 class TestConverter(unittest.TestCase):
 
-    def test_converter_has_something(self):
-        self.
+    def test_converter_has_dictionary(self):
+        converter = Converter({'usd': 1, 'eur': 0.94})
+        self.assertIsInstance(converter, Converter)
 
 
 if __name__ == '__main__':
--- CurrencyConverter.py
+++ CurrencyConverter.py
@@ -1,3 +1,3 @@
 class Converter():
     def __init__(self, dictionary):
-        self.dictionary = []
+        self.dictionary = dictionary
